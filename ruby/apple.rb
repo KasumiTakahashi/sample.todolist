a637c55a70e567192309f5bb6659d412ea77f18a
KasumiTakahashi <nebel.hb@gmail.com>
2021-04-07 02:01:59 +0000

    db:seed

---
+++ ruby/apple.rb
@@ -0,0 +1,10 @@
+apple = "Yamanas"
+
+if  apple == "Aomori"
+  puts "このリンゴは青森県さんです。"
+elsif apple == "Nagano"
+    puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+    puts "このリンゴは青森県産でも、長野県産でもありません。"
+
+end
---
+++ ruby/lesson4.rb
@@ -0,0 +1,2 @@
+name = "Kasumi takahashi"
+puts name
